2760f68e2347f4ec9b664315c4a1a89371760aab
Edgard <edgardmessias@gmail.com>
2021-05-07 20:04:37 -0300

    fix: Fixed getAllMessagesInChat function when chat not found

--- src/lib/wapi/functions/get-all-messages-in-chat.js
+++ src/lib/wapi/functions/get-all-messages-in-chat.js
@@ -15,28 +15,29 @@
  * along with WPPConnect.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-export function getAllMessagesInChat(
+export async function getAllMessagesInChat(
   id,
   includeMe,
   includeNotifications,
   done
 ) {
-  const chat = WAPI.getChat(id);
+  var chat = await WAPI.sendExist(id);
   let output = [];
-  const messages = chat.msgs._models;
+  if (!chat.erro) {
+    const messages = chat.msgs._models;
+    for (const i in messages) {
+      if (i === 'remove') {
+        continue;
+      }
+      const messageObj = messages[i];
 
-  for (const i in messages) {
-    if (i === 'remove') {
-      continue;
+      let message = WAPI.processMessageObj(
+        messageObj,
+        includeMe,
+        includeNotifications
+      );
+      if (message) output.push(message);
     }
-    const messageObj = messages[i];
-
-    let message = WAPI.processMessageObj(
-      messageObj,
-      includeMe,
-      includeNotifications
-    );
-    if (message) output.push(message);
   }
   if (done !== undefined) done(output);
   return output;
