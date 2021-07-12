5fc0ca18496b4ed69ec3d3ec485beffaa8bda267
Edgard <edgardmessias@gmail.com>
2021-07-12 18:30:22 -0300

    fix!: Atualizado versão do puppeteer

---
+++ src/types/puppeteer.d.ts
@@ -0,0 +1,28 @@
+/*
+ * This file is part of WPPConnect.
+ *
+ * WPPConnect is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WPPConnect is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with WPPConnect.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+import * as puppeteer from 'puppeteer';
+
+declare module 'puppeteer' {
+  interface ChromeArgOptions extends puppeteer.BrowserLaunchArgumentOptions {}
+  interface FetcherOptions extends puppeteer.BrowserFetcherOptions {}
+  interface LaunchOptions
+    extends puppeteer.BrowserLaunchArgumentOptions,
+      puppeteer.LaunchOptions,
+      puppeteer.BrowserConnectOptions {}
+}
