3bb5906ca15c03f4a6dc7154281b21fa80ba56e7
claudiorodrigozh <claudio@evocorp.com.br>
2021-08-13 09:19:36 -0300

    fix: Falha ao encerrar sessão no whatsapp beta

--- src/controllers/browser.ts
+++ src/controllers/browser.ts
@@ -111,9 +111,7 @@ export async function injectApi(page: Page) {
   const injected = await page
     .evaluate(() => {
       // @ts-ignore
-
       localStorage.setItem('md-opted-in', 'false');
-      
       return (
         typeof window.WAPI !== 'undefined' &&
         typeof window.Store !== 'undefined'
