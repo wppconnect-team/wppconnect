b958442da6eb5e94225ca5cdb4cbc160116b3449
Edgard <edgardmessias@gmail.com>
2021-09-28 21:26:12 -0300

    fix: Fixed getMessages for beta multidevice (fix #515)

--- src/lib/wapi/functions/get-messages.js
+++ src/lib/wapi/functions/get-messages.js
@@ -16,68 +16,10 @@
  */
 
 export async function getMessages(chatId, params = {}, serialize = true) {
-  // Definição de valores padrões
-  if (typeof params === 'undefined') {
-    params = {};
-  }
-  if (typeof params.count === 'undefined') {
-    params.count = 20;
-  }
-  if (typeof params.direction === 'undefined') {
-    params.direction = 'before';
-  }
-
-  // Corrige o ID do chat
-  if (typeof chatId === 'object' && chatId._serialized) {
-    chatId = chatId._serialized;
-  }
-
-  const chat = window.Store.Chat.get(chatId);
-  if (!chat) {
-    throw {
-      error: true,
-      code: 'chat_not_found',
-      message: 'Chat not found',
-    };
-  }
-
-  const chatWid = Store.WidFactory.createWid(chatId);
-
-  // É valido apenas before e after
-  let direction = 'before';
-  if (params.direction === 'after') {
-    direction = 'after';
-  }
-
-  const queryData = {
-    remote: chatWid,
-    count: params.count,
-    owner: params.fromMe,
-    direction: direction,
-  };
-
-  // Caso tenha informado o ID da mensagem, a consulta se realizará a partir dela
-  if (params.id) {
-    try {
-      const msgKey = window.Store.MsgKey.fromString(params.id);
-      if (msgKey) {
-        queryData.id = msgKey.id;
-        queryData.fromMe = msgKey.fromMe;
-        queryData._serialized = msgKey._serialized;
-      }
-    } catch (error) {
-      queryData.id = params.id;
-    }
-  } else {
-    // Pesquisa sem ID sempre trará mais uma mensagem do que solicitado
-    queryData.count--;
-  }
-
-  const msgs = await Store.msgFindQuery(direction, queryData);
-  console.log(msgs);
+  const msgs = await WPP.chat.getMessages(chatId, params);
 
   if (!Array.isArray(msgs)) {
-    const error = new Error(`Failed to fetch messages for ${chat.id}`);
+    const error = new Error(`Failed to fetch messages for ${chatId}`);
 
     Object.assign(error, msgs);
 
