baed510b63a48c827a543266ab0dd2932521e465
Edgard <edgardmessias@gmail.com>
2021-12-08 21:51:29 -0300

    fix: Fixed setGroupProperty for Beta MultiDevice (fix #670)

--- src/lib/wapi/functions/set-group-property.js
+++ src/lib/wapi/functions/set-group-property.js
@@ -50,6 +50,8 @@ export async function setGroupProperty(groupId, property, value) {
 
   if (property === 'ephemeral') {
     value = value ? 604800 : 0;
+  } else {
+    value = value ? 1 : 0; // MD uses number
   }
 
   const result = await Store.sendSetGroupProperty(
