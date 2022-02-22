9cc34a5fddef52b01feba6194fff31aa19b388f0
Edgard <edgardmessias@gmail.com>
2022-02-22 20:48:14 -0300

    fix: Fixed getGroupMembersIds and getGroupMembers for large chats (fix #892)

--- src/api/layers/group.layer.ts
+++ src/api/layers/group.layer.ts
@@ -48,7 +48,10 @@ export class GroupLayer extends RetrieverLayer {
   public async getGroupMembersIds(groupId: string): Promise<Id[]> {
     return evaluateAndReturn(
       this.page,
-      (groupId: string) => WAPI.getGroupParticipantIDs(groupId),
+      (groupId: string) =>
+        Promise.resolve(WPP.group.getParticipants(groupId)).then(
+          (participants) => participants.map((p) => p.id as any)
+        ),
       groupId
     );
   }
--- src/lib/wapi/functions/get-group-participant-ids.js
+++ src/lib/wapi/functions/get-group-participant-ids.js
@@ -18,8 +18,8 @@
 import { _getGroupParticipants } from './get-group-participants';
 
 export async function getGroupParticipantIDs(groupId, done) {
-  const output = (await _getGroupParticipants(groupId)).map(
-    (participant) => participant.id
+  const output = await Promise.resolve(WPP.group.getParticipants()).then(
+    (participants) => participants.map((p) => p.id)
   );
 
   if (done !== undefined) done(output);
