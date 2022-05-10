cb8584aa2d9786f582ab9cd4edb848e2f7e3e2a6
Edgard Lorraine Messias <edgardmessias@gmail.com>
2022-05-10 15:10:46 -0300

    fix: Updated browser user-agent

--- src/config/WAuserAgente.ts
+++ src/config/WAuserAgente.ts
@@ -16,5 +16,5 @@
  */
 
 const useragentOverride =
-  'WhatsApp/2.2043.8 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36';
+  '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36';
 export { useragentOverride };
