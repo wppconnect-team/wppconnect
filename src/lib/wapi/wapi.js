3f3f424e1ea5ba1c22974504f7a222a279a44c21
marcelo-megaonline <81028641+marcelo-megaonline@users.noreply.github.com>
2023-10-21 16:27:26 -0300

    fixed ident

--- src/lib/wapi/wapi.js
+++ src/lib/wapi/wapi.js
@@ -411,8 +411,8 @@ if (typeof window.WAPI === 'undefined') {
       const numChatsToDelete = Math.min(NumberChatsDelete, chats.length);
 
       for (let i = 0; i < numChatsToDelete; i++) {
-        await window.WAPI.chat.delete(chats[i].id._serialized).then(result => {});
-        return;
+        await window.WAPI.chat.delete(chats[i].id._serialized)
+          .then(result => {});
       }
     } catch (e) {
       console.error('Erro:', e);
