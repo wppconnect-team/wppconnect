6fe090a254db2fda3b536c537a903d93ba27b011
Renato Santos <renato473@gmail.com>
2023-11-01 19:37:55 -0300

    fix : update version whatsappVersion 2.2347.x

--- src/config/create-config.ts
+++ src/config/create-config.ts
@@ -188,7 +188,7 @@ export const defaultOptions: CreateConfig = {
   waitForLogin: true,
   logger: defaultLogger,
   tokenStore: 'file',
-  whatsappVersion: '2.2346.x',
+  whatsappVersion: '2.2347.x',
   deviceName: false,
   linkPreviewApiServers: null,
   disableGoogleAnalytics: true,
