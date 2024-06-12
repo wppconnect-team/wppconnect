22ca707c17a66441a4c19098d238899992a7c4bc
icleitoncosta <cleitoncosta83@gmail.com>
2024-06-11 21:27:02 -0300

    fix: Downgrade whatsappVersion to 2.2413.x

--- src/config/create-config.ts
+++ src/config/create-config.ts
@@ -193,7 +193,7 @@ export const defaultOptions: CreateConfig = {
   waitForLogin: true,
   logger: defaultLogger,
   tokenStore: 'file',
-  whatsappVersion: '2.3000.1014014368-alpha',
+  whatsappVersion: '2.2413.x',
   deviceName: false,
   linkPreviewApiServers: null,
   disableGoogleAnalytics: true,
