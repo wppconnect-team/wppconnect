6ce0e2c58f0e7d213bac7664615ab8f9a58b2cdb
gekkedev <17025257+gekkedev@users.noreply.github.com>
2024-06-12 12:07:37 +0200

    linting

--- src/api/model/incoming-call.ts
+++ src/api/model/incoming-call.ts
@@ -17,18 +17,18 @@
 
 export interface IncomingCall {
   /** alphanumeric ID of the call, can e.g. usable for hanging up */
-  id: string,
+  id: string;
   /** ID of the caller, can be used to message them directly */
-  peerJid: string,
+  peerJid: string;
   /** Epoch timestamp (seconds) */
-  offerTime: number,
-  isVideo: boolean,
-  isGroup: boolean,
-  groupJid: string | null,
-  canHandleLocally: boolean,
-  outgoing: boolean,
-  isSilenced: boolean,
-  offerReceivedWhileOffline: boolean,
-  webClientShouldHandle: boolean,
+  offerTime: number;
+  isVideo: boolean;
+  isGroup: boolean;
+  groupJid: string | null;
+  canHandleLocally: boolean;
+  outgoing: boolean;
+  isSilenced: boolean;
+  offerReceivedWhileOffline: boolean;
+  webClientShouldHandle: boolean;
   participants: any[]
 }
