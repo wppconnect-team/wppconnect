e2fcd6cc19b8014820edd0cebe0aee65833ed610
icleitoncosta <cleitoncosta83@gmail.com>
2025-03-19 12:34:45 -0300

    fix: Upgrade whatsappVersion

--- src/config/create-config.ts
+++ src/config/create-config.ts
@@ -212,7 +212,7 @@ export const defaultOptions: CreateConfig = {
   waitForLogin: true,
   logger: defaultLogger,
   tokenStore: 'file',
-  whatsappVersion: '2.3000.10187x',
+  whatsappVersion: '2.3000.10208x',
   deviceName: false,
   linkPreviewApiServers: null,
   disableGoogleAnalytics: true,
