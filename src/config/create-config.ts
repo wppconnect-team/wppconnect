cf04c5d3cba1778cacba4db101b4274e34278912
Renato Santos <renato473@gmail.com>
2025-11-10 22:33:20 -0300

    [update] version whatsapp web (#2609)

--- src/config/create-config.ts
+++ src/config/create-config.ts
@@ -212,7 +212,7 @@ export const defaultOptions: CreateConfig = {
   waitForLogin: true,
   logger: defaultLogger,
   tokenStore: 'file',
-  whatsappVersion: '2.3000.10271x',
+  whatsappVersion: '2.3000.10294x',
   deviceName: false,
   linkPreviewApiServers: null,
   disableGoogleAnalytics: true,
